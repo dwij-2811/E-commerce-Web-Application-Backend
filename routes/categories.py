ef8b3ec28cb04209c4bb3b5520d9bc4ce8df7d28
Dwij Shah <dwij2811@gmail.com>
2023-09-19 21:25:54 -0600

    added postion

--- routes/categories.py
+++ routes/categories.py
@@ -57,6 +57,7 @@ def delete_category(category_id):
 def add_category():
     try:
         new_category = request.json
+        print (new_category)
         if not new_category:
             return jsonify({"error": "No category data provided"}), 400
 
@@ -66,13 +67,14 @@ def add_category():
                 cursor = connection.cursor()
 
                 insert_query = """
-                INSERT INTO categories (id, name, products)
-                VALUES (%s, %s, %s) RETURNING id;
+                INSERT INTO categories (id, name, products, position)
+                VALUES (%s, %s, %s, %s) RETURNING id;
                 """
                 cursor.execute(insert_query, (
                     new_category["id"],
                     new_category["name"],
                     new_category["products"],
+                    new_category["position"],
                 ))
                 new_category_id = cursor.fetchone()[0]
                 connection.commit()
@@ -94,7 +96,7 @@ def get_all_categories():
         connection = psycopg2.connect(**db_params)
         cursor = connection.cursor()
 
-        select_query = "SELECT * FROM categories ORDER BY name;"
+        select_query = "SELECT * FROM categories ORDER BY position;"
         cursor.execute(select_query)
         categories = cursor.fetchall()
 
@@ -107,9 +109,32 @@ def get_all_categories():
                 "id": category[0],
                 "name": category[1],
                 "products": category[2],
+                "position": category[3],
             }
             categories_list.append(category_dict)
 
         return jsonify(categories_list)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@categories_bp.route('/update_category_positions', methods=['POST'])
+def update_category_positions():
+    try:
+        new_positions = request.get_json()
+
+        connection = psycopg2.connect(**db_params)
+        cursor = connection.cursor()
+
+        for category in new_positions:
+            cursor.execute(
+                "UPDATE categories SET position = %s WHERE id = %s",
+                (category["position"], category["id"])
+            )
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        return jsonify({'message': 'Category positions updated successfully'}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
